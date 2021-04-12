996afc4b2152ace76edecd813b1363ee9b958370
Itzel01 <itzel.areymi.rendon@gmail.com>
2021-04-12 11:15:17 -0400

    files are linked and ready to go

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const methodOverride = require('method-override');
+
+const blogRouter = require('./routers/blogRouter');
+const userRouter = require('./routers/userRouter');
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
+
+app.use('/blogs', blogRouter)
+app.use('/users', userRouter)
 
 
 app.listen(PORT, () => {console.log(`Server starting on port ${PORT}`)});
