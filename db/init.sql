dcdf2102710a66b927beac6fad62ffa01522557b
LivCodes <oliviareeddev@gmail.com>
2021-04-13 12:26:36 -0400

    updated sql

--- db/init.sql
+++ db/init.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS blogs CASCADE;
 DROP TABLE IF EXISTS posts CASCADE;
 DROP TABLE IF EXISTS playlists CASCADE;
+DROP TABLE IF EXISTS bookmarks CASCADE;
 
 -- CREATE TABLE users
 CREATE TABLE users (id SERIAL PRIMARY KEY, user_name TEXT, email TEXT, encypted_password CHAR(60));
@@ -13,28 +14,28 @@ INSERT INTO users (user_name, email, encypted_password) VALUES
 	('Itzel', 'Itzel@twitch.com', '123456');
 	
 -- CREATE TABLE blogs
-CREATE TABLE blogs (id SERIAL PRIMARY KEY, title TEXT, blog_content TEXT, user_id INT REFERENCES users(id));
+CREATE TABLE blogs (id SERIAL PRIMARY KEY, title TEXT, blog_content TEXT, created_at TIMESTAMP WITH TIME ZONE, user_id INT REFERENCES users(id));
 
 -- INSERT some data into blogs
-INSERT INTO blogs (user_id, title, blog_content) VALUES
-	(1, 'Ten Reasons Why People Love Anything ', 'content'),
-	(2, 'Why Is Ice Cream Considered Underrated?', 'content'),
-	(3, 'Things You Did not Know About Soda', 'content'),
-	(1, 'You Will Never Believe These Bizarre Truth Of Video Games', 'content'),
-	(2, 'The best about Javascript', 'content'),
-	(3, 'Why every loves a good CSS joke', 'content');
+INSERT INTO blogs (user_id, title, blog_content, created_at) VALUES
+	(1, 'Ten Reasons Why People Love Anything ', 'content', now()),
+	(2, 'Why Is Ice Cream Considered Underrated?', 'content', now()),
+	(3, 'Things You Did not Know About Soda', 'content', now()),
+	(1, 'You Will Never Believe These Bizarre Truth Of Video Games', 'content', now()),
+	(2, 'The best about Javascript', 'content', now()),
+	(3, 'Why every loves a good CSS joke', 'content', now());
 
 -- CREATE TABLE posts
-CREATE TABLE posts (id SERIAL PRIMARY KEY, post_content TEXT, user_id INT REFERENCES users(id), likes INT, shares INT);
+CREATE TABLE posts (id SERIAL PRIMARY KEY, post_content TEXT, created_at TIMESTAMP WITH TIME ZONE, user_id INT REFERENCES users(id));
 
 -- INSERT some data into posts
-INSERT INTO posts (user_id, post_content, likes, shares) VALUES
-	(1,  'content', 2, 5),
-	(2,  'content', 6, 8),
-	(3,  'content', 3, 2),
-	(1,  'content', 6, 9),
-	(2,  'content', 1, 5),
-	(3,  'content', 9, 1);
+INSERT INTO posts (user_id, post_content, created_at) VALUES
+	(1,  'content', now()),
+	(2,  'content', now()),
+	(3,  'content', now()),
+	(1,  'content', now()),
+	(2,  'content', now()),
+	(3,  'content', now());
 	
 -- CREATE TABLE playlists
 CREATE TABLE playlists (id SERIAL PRIMARY KEY, user_id INT REFERENCES users(id), title TEXT,  artist TEXT, media_link TEXT);
