ad34116da0e8b1f26c43428eb1f99e1b8783026c
Jarr1t <aliceajarrit@gmail.com>
2021-04-13 21:39:10 -0400

    Auth works

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const methodOverride = require('method-override');
+const session = require('express-session')
 
+const registerRouter = require('./routers/registerRouter')
+const loginRouter = require('./routers/loginRouter')
 const blogRouter = require('./routers/blogRouter');
 const userRouter = require('./routers/userRouter');
 
@@ -11,11 +14,41 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 
+app.use(session({
+    secret: 'wowwowwow',
+    resave: false,
+    saveUninitialized: false,
+    name: 'Not a real Cookie'
+}))
+
 app.use('/', (req, res, next) => {
     console.log(req.method, req.url)
     next()
 })
 
+// app.get('/', (req, res) => {
+//     res.redirect('/login')
+// })
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+// app.get("/users", (req, res) => {
+//     let {user} = req.session
+//     if(user) {
+//       res.render("users", {user})
+//     } else {
+//       res.redirect("/login")
+//     }
+// });
+
+app.use('/login', loginRouter)
+app.use('/register', registerRouter)
 app.use('/blogs', blogRouter)
 app.use('/users', userRouter)
 
